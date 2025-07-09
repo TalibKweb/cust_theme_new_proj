a22a7a1bcd1c8dee57257cb3fd0e2045944c7f2f
TalibKweb <talib.kwebmaker@gmail.com>
2025-07-09 18:23:54 +0530

    added some props to the scrolling library initialization

--- wp-content/themes/notandas/js/additional_script.js
+++ wp-content/themes/notandas/js/additional_script.js
@@ -89,11 +89,17 @@ document.addEventListener('DOMContentLoaded', () => {
 console.log('Additional Script JS Connected!')
 
 // Initialize Lenis
-const lenis = new Lenis();
+const lenis = new Lenis({
+    // lerp: 0.1,
+    duration: 2.5,
+    autoRaf: true,
+    anchors: true,
+    // infinite: true,
+});
 
 // Listen for the scroll event and log the event data
 lenis.on('scroll', (e) => {
-  console.log(e);
+//   console.log(e);
 });
 
 // Use requestAnimationFrame to continuously update the scroll
